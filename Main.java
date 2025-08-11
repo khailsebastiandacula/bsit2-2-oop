3f1eb651ecde4cfcf7d88f943455517281fabae9
Khail Sebastian Dacula <ksdacula52420@liceo.edu.ph>
2025-08-11 22:43:30 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,18 @@
-public class Main {
+import java.util.Scanner;
+
+class SumFiveNumbers {
     public static void main(String[] args) {
-        Student s1 = new Student("Alice", 20, "BSIT", 85.0f, 90.0f, 88.0f);
-        Student s2 = new Student("Bob", 19, "BSCS", 92.0f, 95.0f, 89.0f);
-        Student s3 = new Student("Charlie", 21, "BSIT", 65.0f, 70.0f, 68.0f);
+        Scanner scanner = new Scanner(System.in);
+        int totalNumber = 0;
 
-        Student[] students = { s1, s2, s3 };
-        int passingCount = 0;
-
-        for (Student stdnts : students) {
-            stdnts.displayInfo();
-            if (stdnts.isPassing()) {
-                passingCount++;
-            }
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = scanner.nextInt();
+            totalNumber += number;
         }
 
-        System.out.println("Summary: " + passingCount + " out of " +
-                students.length + " students are passing.");
+        System.out.println("Total Number: " + totalNumber);
+
+        scanner.close();
     }
 }
