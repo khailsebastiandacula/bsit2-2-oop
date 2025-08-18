da6bdf620d1233312e6cde5629fc101b98fda566
Khail Sebastian Dacula <ksdacula52420@liceo.edu.ph>
2025-08-18 11:51:17 +0800

    activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) {
+        GradeCalculator gc = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double average = gc.calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String letterGrade = gc.getLetterGrade(average);
+
+    System.out.println("studentName, average");
+    gc.displayResult(studentName,average);
+
+    System.out.println("studentName, average, letterGrade");
+    gc.displayResult(studentName,average,letterGrade);
     }
 }
