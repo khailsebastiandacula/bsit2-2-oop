e8cb77a0aaf9952ca1f121f592c1527848fabd02
Khail Sebastian Dacula <ksdacula52420@liceo.edu.ph>
2025-09-03 23:22:54 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,7 @@
-import java.util.Scanner;
+public static void main(String[] args) {
+        LibraryManager library = new LibraryManager();
 
-class SumFiveNumbers {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int totalNumber = 0;
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            int number = scanner.nextInt();
-            totalNumber += number;
-        }
-
-        System.out.println("Total Number: " + totalNumber);
-
-        scanner.close();
+        library.showBooks();
+        library.addBook();
+        library.removeBook();
     }
-}
