1296a0a4da60952dbd9a2858b116e142b3abee0a
Khail Sebastian Dacula <ksdacula52420@liceo.edu.ph>
2025-09-15 16:26:51 +0800

    Create Dog.java

---
+++ src/Dog.java
@@ -0,0 +1,15 @@
+class Dog extends Pet implements Trainable {
+    public Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Woof! Woof!");
+    }
+
+    @Override
+    public void performTrick() {
+        System.out.println("Training " + name + ": Sits and shakes");
+    }
+}
