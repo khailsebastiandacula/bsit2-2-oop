53bd3a97715590e868d3df18682942398de43fa6
Khail Sebastian Dacula <ksdacula52420@liceo.edu.ph>
2025-09-23 12:23:15 +0800

    Create DVD.java

---
+++ src/DVD.java
@@ -0,0 +1,18 @@
+class DVD extends Media {
+    private String director;
+
+    public DVD(String title, String itemId, String director) {
+        super(title, itemId);
+        this.director = director;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "DVD";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("DVD: \"" + title + "\" directed by " + director + " (ID: " + itemId + ")");
+    }
+}
