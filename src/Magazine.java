c01c1affa49181bcdb6957eed04b5ab7012d9fb9
Khail Sebastian Dacula <ksdacula52420@liceo.edu.ph>
2025-09-23 12:23:33 +0800

    Create Magazine.java

---
+++ src/Magazine.java
@@ -0,0 +1,18 @@
+class Magazine extends Media {
+    private int issueNumber;
+
+    public Magazine(String title, String itemId, int issueNumber) {
+        super(title, itemId);
+        this.issueNumber = issueNumber;
+    }
+
+    @Override
+    public String getMediaType() {
+        return "Magazine";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Magazine: \"" + title + "\" Issue #" + issueNumber + " (ID: " + itemId + ")");
+    }
+}
